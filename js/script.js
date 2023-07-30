da1794d70f821ba59a89962f8572fa89b4c9d403
babytype <babytype3000@gmail.com>
2023-07-30 22:13:35 +0300

    Секция отзывы

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
-$(document).ready(function(){
-    $('.hamburger').on('click',function(){
-        $('.mobile-menu').addClass('is-active');
-    });
+$(document).ready(function() {
 
-    $('.mobile-menu-close').on('click', function(){
-        $('.mobile-menu').removeClass('is-active');
-    });
+	$('.hamburger').on('click', function() {
+		$('.mobile-menu').addClass('is-active');
+	});
+
+	$('.mobile-menu-close').on('click', function() {
+		$('.mobile-menu').removeClass('is-active');
+	});
+
+	// Reviews Swiper Start
+	const reviewsSwiper = new Swiper('.reviews-swiper', {
+		loop: true,
+		navigation: {
+		  nextEl: '.reviews-swiper .swiper-button-next',
+		  prevEl: '.reviews-swiper .swiper-button-prev',
+		},
+		speed: 1000
+	});
+	// Reviews Swiper End
+
 });
